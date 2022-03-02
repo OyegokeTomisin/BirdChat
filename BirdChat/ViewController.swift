074de52dbb06eba3bd5cdf737e0cc1d899456dce
tomisin <tomisin18@gmail.com>
2022-03-02 13:31:48 +0100

    Initialize project

---
+++ BirdChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirdChat
+//
+//  Created by Oyegoke Oluwatomisin on 02/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

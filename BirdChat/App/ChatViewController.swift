cf61c5049ce3af552f61dc4293c6d89c74133f5a
tomisin <tomisin18@gmail.com>
2022-03-05 11:50:48 +0100

    Setup audio recoding layout in chat controller

--- BirdChat/App/ChatViewController.swift
+++ BirdChat/App/ChatViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 final class ChatViewController: UIViewController {
     
     let messageInputField = MessageInputField()
+    let audioInputField = AudioInputField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,20 +20,49 @@ final class ChatViewController: UIViewController {
         messageInputField.constrainHeight(constant: 40)
         messageInputField.anchor(top: nil, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 20, bottom: 20, right: 20))
         
-        messageInputField.sendButton.addTarget(self, action: #selector(sendMessage), for: .touchUpInside)
+        view.backgroundColor = .white
+        view.addSubviews(audioInputField)
+        audioInputField.anchor(top: nil, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 20, bottom: 20, right: 20))
+        
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard)))
+        messageInputField.sendButton.addTarget(self, action: #selector(sendMessage), for: .touchUpInside)
+        messageInputField.recordButton.addTarget(self, action: #selector(startRecording), for: .touchUpInside)
+        audioInputField.recordingSendButton.addTarget(self, action: #selector(sendRecording), for: .touchUpInside)
+        audioInputField.recordingStopButton.addTarget(self, action: #selector(stopRecording), for: .touchUpInside)
+        audioInputField.recordingCancelButton.addTarget(self, action: #selector(cancelRecording), for: .touchUpInside)
+        
+        cancelRecording()
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(sender:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
-    @objc func dismissKeyboard() {
-        messageInputField.texView.resignFirstResponder()
+    @objc func startRecording() {
+        messageInputField.isHidden = true
+        audioInputField.isHidden = false
+    }
+    
+    @objc func cancelRecording() {
+        messageInputField.isHidden = false
+        audioInputField.isHidden = true
+    }
+    
+    @objc func stopRecording() {
+        
+    }
+    
+    @objc func sendRecording() {
+        cancelRecording()
     }
     
     @objc func sendMessage() {
         messageInputField.sendMessage()
-        debugPrint(messageInputField.texView.text)
+        // debugPrint(messageInputField.texView.text)
+    }
+    
+    @objc func dismissKeyboard() {
+        stopRecording()
+        messageInputField.texView.resignFirstResponder()
     }
     
     @objc func keyboardWillShow(sender: NSNotification) {
